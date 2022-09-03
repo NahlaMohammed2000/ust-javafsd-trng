bf7d9fce9deff361a0fb389e708e4945ed3184ee
NahlaMohammed2000 <112387578+NahlaMohammed2000@users.noreply.github.com>
2022-09-03 10:53:52 +0530

    Add files via upload

---
+++ Java/InstanceofDemo3.java
@@ -0,0 +1,11 @@
+package com.ust;
+class A{}
+class B extends A{}
+public class InstanceofDemo3 {
+	public static void main(String[] args) {
+		B objOfClassB=new B();
+		System.out.println("objOfClassB is an instance of B: "+(objOfClassB instanceof B));
+		System.out.println("objOfClassB is an instance of parent class A: "+(objOfClassB instanceof A));
+		
+	}
+}
