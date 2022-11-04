e714a45760d37eb02df11bdef975ca9c66ca08dc
NahlaMohammed2000 <112387578+NahlaMohammed2000@users.noreply.github.com>
2022-11-04 17:42:14 +0530

    Add files via upload

--- Java/Student.java
+++ Java/Student.java
@@ -1,40 +1,42 @@
-package com.ust;
-//firstName, billOfThePatient
+import java.util.Objects;
+
 public class Student {
-int sID;
-String name;
-int age; //instance variables are assigned default value 0
-String course;
-static int count;//value of count will be same since static variable
+    String name;
+    int age,rollNo;
+    public String getName() {
+        return name;
+    }
 
-//public Student() {
-	//Constructors dont have return type
-//}
+    public int getAge() {
+        return age;
+    }
 
-{
-int z;
-System.out.println(z=7);
-}
-//in the output block statments get executed first
+    public int getrollNo() {
+        return rollNo;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && rollNo == student.rollNo && name.equals(student.name);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, rollNo);
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
-public static void main(String args[]) {
-	float f=12.45f;
-	int x=5;
-	String str;
-	Student s = new Student();    //Default constructor/zero arg constructor
-	Student s1 = new Student(); 
-	Student s2 = new Student(); 
-	s.age=22;
-System.out.println(s.age);
-System.out.println(x);
-s.count=15;
-s1.age=25;
-System.out.println(s1.age);
-System.out.println(s.count);
-System.out.println(s1.count);//since count is global variable
+    public void setAge(int age) {
+        this.age = age;
+    }
 
-}
+    public void setrollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
 }
